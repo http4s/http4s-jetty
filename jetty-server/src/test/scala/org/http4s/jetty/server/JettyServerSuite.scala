91c244461464d70bedfb66e0bfc280121c9eda49
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2023-06-15 20:23:46 -0600

    Added extra logging

--- jetty-server/src/test/scala/org/http4s/jetty/server/JettyServerSuite.scala
+++ jetty-server/src/test/scala/org/http4s/jetty/server/JettyServerSuite.scala
@@ -119,9 +119,7 @@ class JettyServerSuite extends CatsEffectSuite {
   }
 
   jettyServer.test("Timeout should fire on timeout") { server =>
-    get(server, "/never").map{ x=>
-        println(">>>" + x)
-        x.contains("Error 500 AsyncContext timeout")}
+    get(server, "/never").map{_.contains("Error 500 AsyncContext timeout")}
   }
 
   jettyServer.test("Timeout should execute the service task on the service executor") { server =>
